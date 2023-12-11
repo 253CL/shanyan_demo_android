e1911a6bc77650d40ca2433c742121f3976a8c99
他乡之客 <1107280443@qq.com>
2023-12-11 13:36:18 +0800

    更新2.3.6.4版本SDK

--- app/src/main/java/com/login/demo/MyApplication.java
+++ app/src/main/java/com/login/demo/MyApplication.java
@@ -14,11 +14,6 @@ public class MyApplication extends Application {
 
         //SDK配置debug开关 （必须放在初始化之前，开启后可打印SDK更加详细日志信息）
         OneKeyLoginManager.getInstance().setDebug(true);
-        //demo示例崩溃信息日志收集，开发者不需要添加
-        //BuglyUtils.setBugly(getApplicationContext());
-
-        //demo示例内存泄漏检测，开发者不需要添加
-        // LeakCanaryUtils.setupLeakCanary(getApplicationContext());
     }
 
 
